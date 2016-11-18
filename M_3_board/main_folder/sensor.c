9bdeea08ba8e25f0f842fd8cb369bb835178c10a
Agnus <tenebris.agnus@yandex.ru>
2016-11-18 23:53:38 +0300

    Написала функции для датчиков отделения, удалила ненужные функции, связанные с поршнями

--- M_3_board/main_folder/sensor.c
+++ M_3_board/main_folder/sensor.c
@@ -4,37 +4,34 @@
  *  Created on: 26 марта 2016 г.
  *      Author: developer
  */
-
 #include <avr/io.h>
-#include <util/delay.h>
-#include <stdio.h>
 #include <stdbool.h>
 
-#define ps_port PORTA
-#define ps_ppin PINA
-#define ps_ddr DDRA
-#define ps_pin_1 3
-#define ps_pin_2 4
-#define ps_pin_3 5
+#include "hal/defines.h"
 
 void sensor_init (){
-	ps_ddr &= ~(1<< ps_pin_1);
-	ps_ddr &= ~(1<< ps_pin_2);
-	ps_ddr &= ~(1<< ps_pin_3);
+	SEPARATION_SENSOR_DDR &= ~(1<< SEPARATION_SENSOR_1_PIN);
+	SEPARATION_SENSOR_DDR &= ~(1<< SEPARATION_SENSOR_2_PIN);
+	SEPARATION_SENSOR_DDR &= ~(1<< SEPARATION_SENSOR_3_PIN);
 }
 
-bool porsh_sensor_1 (){
-	if ((ps_ppin & (1<< ps_pin_1)) != 0)
-		return true;
-	else return false;
-}
-bool porsh_sensor_2 (){
-	if ((ps_ppin & (1<< ps_pin_2)) != 0)
-		return true;
-	else return false;
-}
-bool porsh_sensor_3 (){
-	if ((ps_ppin & (1<< ps_pin_3)) != 0)
-		return true;
-	else return false;
+bool separation_sensor (uint8_t sensor_number){
+	switch (sensor_number){
+	case 1:
+		if ((SEPARATION_SENSOR_PPIN & (1<< SEPARATION_SENSOR_1_PIN)) == 0)
+			return true;
+		else return false;
+		break;
+	case 2:
+		if ((SEPARATION_SENSOR_PPIN & (1<< SEPARATION_SENSOR_2_PIN)) == 0)
+			return true;
+		else return false;
+		break;
+	case 3:
+		if ((SEPARATION_SENSOR_PPIN & (1<< SEPARATION_SENSOR_3_PIN)) == 0)
+			return true;
+		else return false;
+		break;
+	}
+	return false;
 }
