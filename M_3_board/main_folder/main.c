09999203f99e32612ca4c2409c2258d912464193
Agnus <tenebris.agnus@yandex.ru>
2017-01-18 19:46:41 +0300

    Убрала и перенесла наметки кода, комментарии и строчки, оставшиеся с предыдущих тестов

--- M_3_board/main_folder/main.c
+++ M_3_board/main_folder/main.c
@@ -23,30 +23,22 @@
 #include "HC_SR04.h"
 #include "hal/structs.h"
 
-#define CHECK_PRESSURE_MIN 0 //<<========================== add value
-#define CHECK_PRESSURE_MAX 0 //<<========================== add value
-#define CHECK_TEMPERATURE_MIN 0 //<<========================== add value
-#define CHECK_TEMPERATURE_MAX 0 //<<========================== add value
-#define ERROR_BMP180_PRESSURE 0x01
-#define ERROR_BMP180_TEMPERATURE 0x02
-#define ALL_RIGHT  0x00
-
 int main() {
 //INIT
 	DDRG |= (1<<3);
-	//adc_init();
-	//BMP180_init();
+	adc_init();
+	BMP180_init();
 	motor_init();
-	//one_wire_init();
+	one_wire_init();
 	sensor_init();
-	//spi_init();
+	spi_init();
 	time_service_init();
-	//twi_init();
+	twi_init();
 	uart_init();
 	uart_stdio_init();
 	HC_SR04_init();
 //CONST
-	const time_data_t time_porsh = {2,0};//<<========================== add value
+	const time_data_t time_porsh = {2,0};
 //VARIABLE
 	typedef enum {
 		STATE_IN_FIRST_MEASURE,
@@ -62,46 +54,22 @@ int main() {
 	float hight = 0;
 	uint32_t pressure_at_start;
 //CHECK
-	/*описание ошибок (дописать)
-	 * 0x00 - all right*/
-	/*uint8_t check_state;
-	main_packet.BMP180_temperature = BMP180_count_temperature();
-	if((main_packet.BMP180_temperature < CHECK_TEMPERATURE_MIN) && (main_packet.BMP180_temperature > CHECK_TEMPERATURE_MAX)){
-		check_state = ERROR_BMP180_TEMPERATURE;
-	}
-	main_packet.BMP180_pressure = BMP180_count_pressure();
-	if((main_packet.BMP180_pressure < CHECK_PRESSURE_MIN) && (main_packet.BMP180_pressure > CHECK_PRESSURE_MAX)){
-		check_state = ERROR_BMP180_PRESSURE;
-	}
-	if(check_state != ALL_RIGHT){
-		PORTG |= (1<<3);
-		_delay_ms(250);
-		PORTG &= ~(1<<3);
-		_delay_ms(250);
-		PORTG |= (1<<3);
-		_delay_ms(500);
-		PORTG &= ~(1<<3);
-		_delay_ms(250);
-		PORTG |= (1<<3);
-		_delay_ms(250);
-		PORTG &= ~(1<<3);
-	} else PORTG |= (1<<3);*/
+	//-----------------------------------------------------------finish writing
 //BEFORE START
 	//pressure_at_start = //----------------------------- add function
 //BEFORE SEPARATION
+	//-----------------------------------------------------------finish writing
 	while(1){}
 //AFTER SEPARATION
-	//const float hight_at_separation = 2;
-	const float hight_1 = 97000;//(3 * hight_at_separation) / 4;
-	const float hight_2 = 98000;//hight_at_separation / 2;
-	const float hight_3 = 99000;//hight_at_separation / 4;
-
+	const float hight_at_separation = 2;//----------------------------add function
+	const float hight_1 = (3 * hight_at_separation) / 4;
+	const float hight_2 = hight_at_separation / 2;
+	const float hight_3 = hight_at_separation / 4;
 	while (1){
 //MAKE DATA
+		//-----------------------------------------finish writing
 		HC_SR04_trip_check();
 		adc_read(&main_packet.MPX5100_pressure);
-
-		printf("%f\n",hight);
 //OTHER ACTIONS
 //PISTONS
 		switch (state_now) {
@@ -109,7 +77,6 @@ int main() {
 		case STATE_IN_FIRST_MEASURE:
 			if (hight >= hight_1){
 				motor_on (1);
-				printf("motor 1 on \n");//-------------------------------------------
 				porsh_1.time_krit = time_sum(time_service_get(), time_porsh);
 				porsh_1.end = true;
 				state_now = STATE_IN_SECOND_MEASURE;
@@ -119,7 +86,6 @@ int main() {
 		case STATE_IN_SECOND_MEASURE:
 			if (hight >= hight_2){
 				motor_on (2);
-				printf("motor 2 on \n");//-------------------------------------------
 				porsh_2.time_krit = time_sum(time_service_get(), time_porsh);
 				porsh_2.end = true;
 				state_now = STATE_IN_THIRD_MEASURE;
@@ -130,7 +96,6 @@ int main() {
 		case STATE_IN_THIRD_MEASURE:
 			if (hight >= hight_3) {
 				motor_on (3);
-				printf("motor 3 on \n");//-------------------------------------------
 				porsh_3.time_krit = time_sum(time_service_get(), time_porsh);
 				porsh_3.end = true;
 				state_now = STATE_AFTER_THIRD_MEASURE;
