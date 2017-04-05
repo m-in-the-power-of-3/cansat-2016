40408df1e43a091ab0b2f1fca27a94e2ead1ce49
Agnus <tenebris.agnus@yandex.ru>
2017-04-05 21:35:02 +0300

    Изменила код в соотетствии с новой библиотекой

--- M_3_board/main_folder/main.c
+++ M_3_board/main_folder/main.c
@@ -53,11 +53,10 @@ int main (){
 
   //TWI
 	rscs_i2c_init();
-	rscs_i2c_set_scl_rate(800);
-
-  //SPI
+	rscs_i2c_set_scl_rate(16);
+  /*//SPI
 	rscs_spi_init();
-	rscs_spi_set_clk(RSCS_BMP280_SPI_FREQ_kHz);
+	rscs_spi_set_clk(RSCS_BMP280_SPI_FREQ_kHz);*/
 
   //TIME
 	rscs_time_init();
@@ -71,7 +70,7 @@ int main (){
 	rscs_ds18b20_t * ds18b20_1 = rscs_ds18b20_init(0);
 
   //BMP180
-	bmp180_init();
+	//bmp180_init();
 
   //HC_SR04
 	HC_SR04_init();
@@ -79,12 +78,14 @@ int main (){
   //BMP280
 	DDRC |= (1<<2);//delete
 	PORTC |= (1<<2);//delete
-
-	rscs_bmp280_descriptor_t * bmp280_descriptor = rscs_bmp280_init();
+	rscs_bmp280_descriptor_t * bmp280_descriptor = rscs_bmp280_initi2c(RSCS_BMP280_I2C_ADDR_HIGH);
 	rscs_bmp280_parameters_t bmp280_parametrs = {RSCS_BMP280_OVERSAMPLING_X16,RSCS_BMP280_OVERSAMPLING_X2,RSCS_BMP280_STANDBYTIME_500MS,RSCS_BMP280_FILTER_X16};
-	rscs_bmp280_setup(bmp280_descriptor,&bmp280_parametrs);
+	rscs_e error3 = -8;
+	while((error3 != 0)){
+		error3 = rscs_bmp280_setup(bmp280_descriptor,&bmp280_parametrs);
+		printf("error 3 = %i \n",error3);
+	}
 	rscs_bmp280_changemode (bmp280_descriptor,RSCS_BMP280_MODE_NORMAL);
-
 //============================================================================
 //CONST
 //============================================================================
@@ -92,16 +93,20 @@ int main (){
 //============================================================================
 //VARIABLE
 //============================================================================
+
 	int32_t raw_press  = 0;
 	int32_t raw_temp = 0;
 
 	int32_t pressure;
 	int32_t temperature;
-
+	rscs_e error1;
+	rscs_e error2;
 	while(1){
-		rscs_bmp280_read(bmp280_descriptor,&raw_press,&raw_temp);
-		rscs_bmp280_calculate(bmp280_calibration_values,raw_press,raw_temp,&pressure,&temperature);
+		error1 = rscs_bmp280_read(bmp280_descriptor,&raw_press,&raw_temp);
+		error2 = rscs_bmp280_calculate(bmp280_calibration_values,raw_press,raw_temp,&pressure,&temperature);
 		printf("temperature = %li\n",temperature);
 		printf("pressure = %li\n",pressure);
+		printf("error 1 = %i \n",error1);
+		printf("error 2 = %i \n",error2);
 	}
 }
